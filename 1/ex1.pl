5fc0338e96382d09e82ac3be06d7839c90247639
Anvar Umarov <arpanetus@protonmail.com>
2019-11-04 22:58:01 +0600

    Add Exercise  1.1

---
+++ 1/ex1.pl
@@ -0,0 +1,15 @@
+/*
+Exercise  1.1
+
+
+1. vINCENT                - atom
+2. Footmassage            - variable
+3. variable23             - atom
+4. Variable2000           - variable
+5. big_kahuna_burger      - atom 
+6. ’big  kahuna  burger’  - atom
+7. big  kahuna  burger    - neither
+8. ’Jules’                - atom
+9. _Jules                 - variable
+10. ’_Jules’              - atom
+*/

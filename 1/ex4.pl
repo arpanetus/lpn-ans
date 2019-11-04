8da58b1670e8e8352eeeaa92b44c2e14f5f5f5bb
Anvar Umarov <arpanetus@protonmail.com>
2019-11-04 23:33:32 +0600

    Add Exercise 1.4

---
+++ 1/ex4.pl
@@ -0,0 +1,13 @@
+/* 
+Exercise 1.4
+*/
+
+killer(butch).
+married(mia, marsellus).
+dead(zed).
+kills(marsellus, X) :- footMassage(mia,X).
+loves(mia, X) :- goodDancer(X).
+eats(jules, X) :- nutricious(X); tasty(X).
+
+
+

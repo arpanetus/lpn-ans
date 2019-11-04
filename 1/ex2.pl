dc539082a42c1d1c65d6751ceca94a4e9b66fdc1
Anvar Umarov <arpanetus@protonmail.com>
2019-11-04 23:12:33 +0600

    Add Exercise 1.2

---
+++ 1/ex2.pl
@@ -0,0 +1,15 @@
+/*
+Exercise 1.2
+
+
+1. loves(Vincent,mia)                 - loves\/2
+2. ’loves(Vincent,mia)’               - atom
+3. Butch(boxer)                       - not a term, since variable can't be a functor
+4. boxer(Butch)                       - boxer\/1
+5. and(big(burger),kahuna(burger))    - and\/2
+6. and(big(X),kahuna(X))              - and\/2
+7. _and(big(X),kahuna(X))             - not a term, since variable can't be a functor
+8. (Butch  kills  Vincent)            - not a term, since there is no comma and functor
+9. kills(Butch  Vincent)              - not a term, since there is no comma
+10. kills(Butch,Vincent               - not a term, since there is no closing paranthesis
+*/
